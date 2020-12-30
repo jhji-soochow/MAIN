f45998802297eef522204e728864e546ee90f341
jhji-soochow <jhji.soochow@gmail.com>
2020-12-30 11:02:24 +0800

    modification

--- src/train_MAIN.sh
+++ src/train_MAIN.sh
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=4 python main.py --trainer trainer --template AIN --n_GPUs 
 # --epochs 400 --print_every 100 --test_every 1000 --n_threads 16 \
 # --save AIN_136x2 --save_results --visdom --ext sep
 
-# 96 * 8 / 4 = 192
+# 96 * 2 = 192
 # CUDA_VISIBLE_DEVICES=3 python main.py --trainer trainer --template AIN --n_GPUs 1 --lr 0.0001 --loss 1*MSE \
 # --data_train DIV2K --data_range 1-800/801-810 --n_colors 1 --scale 2 --batch_size=16 --patch_size 192 --direct_downsampling \
 # --data_test Set5+Set14+Urban100+B100 \
