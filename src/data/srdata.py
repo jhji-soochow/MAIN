91153bbe40f064aa3934bcedfbdb4598a4b5ec7b
jhji-soochow <jhji.soochow@gmail.com>
2020-12-30 12:55:10 +0800

    for genete lr image

--- src/data/srdata.py
+++ src/data/srdata.py
@@ -88,15 +88,28 @@ class SRData(data.Dataset):
         names_hr = sorted(
             glob.glob(os.path.join(self.dir_hr, '*' + self.ext[0]))
         )
-        names_lr = [[] for _ in self.scale]
-        for f in names_hr:
-            filename, _ = os.path.splitext(os.path.basename(f))
-            for si, s in enumerate(self.scale):
-                names_lr[si].append(os.path.join(
-                    self.dir_lr, 'X{}/{}x{}{}'.format(
-                        s, filename, s, self.ext[1]
-                    )
-                ))
+        # if no prepared lr images
+        if self.dir_lr == None:
+            names_lr = None
+        else:
+            names_lr = [[] for _ in self.scale]
+            for f in names_hr:
+                filename, _ = os.path.splitext(os.path.basename(f))
+                for si, s in enumerate(self.scale):
+                    names_lr[si].append(os.path.join(
+                        self.dir_lr, 'X{}/{}x{}{}'.format(
+                            s, filename, s, self.ext[1]
+                        )
+                    ))
+        # names_lr = [[] for _ in self.scale]
+        # for f in names_hr:
+        #     filename, _ = os.path.splitext(os.path.basename(f))
+        #     for si, s in enumerate(self.scale):
+        #         names_lr[si].append(os.path.join(
+        #             self.dir_lr, 'X{}/{}x{}{}'.format(
+        #                 s, filename, s, self.ext[1]
+        #             )
+        #         ))
 
         return names_hr, names_lr
 
@@ -165,7 +178,10 @@ class SRData(data.Dataset):
     def _load_file(self, idx):
         idx = self._get_index(idx)
         f_hr = self.images_hr[idx]
-        f_lr = self.images_lr[self.idx_scale][idx]
+        if self.images_lr is None:
+            f_lr = None
+        else:
+            f_lr = self.images_lr[self.idx_scale][idx]
 
         if self.args.ext.find('bin') >= 0:
             filename = f_hr['name']
@@ -184,25 +200,22 @@ class SRData(data.Dataset):
 
     def get_patch(self, lr, hr):
         scale = self.scale[self.idx_scale]
-        if self.train:
-            lr, hr = common.get_patch(
-                lr, hr,
-                patch_size=self.args.patch_size,
-                scale=scale,
-                multi=(len(self.scale) > 1),
-                input_large=self.input_large
-            )
-            if not self.args.no_augment: lr, hr = common.augment(lr, hr)
-        else:
-            ih, iw = lr.shape[:2]
-            # 为test数据增加噪声
-            # if self.args.test_noisy: lr = common.add_noise(lr)
-            hr = hr[0:ih * scale, 0:iw * scale]
+        if lr is not None:
+            if self.train:
+                lr, hr = common.get_patch(
+                    lr, hr,
+                    patch_size=self.args.patch_size,
+                    scale=scale,
+                    multi=(len(self.scale) > 1),
+                    input_large=self.input_large
+                )
+                if not self.args.no_augment: lr, hr = common.augment(lr, hr)
+            else:
+                ih, iw = lr.shape[:2]
+                hr = hr[0:ih * scale, 0:iw * scale]
 
-        # pdb.set_trace()
-        # 如果是直接降采样，将lr替换成hr的直接降采样，这是一个笨办法，但是不用重新生成数据，可能会慢一点
-        # 这一步在
         if self.args.direct_downsampling:
+            # directly downsample hr for producing lr
             hr = common.crop_for_scale(hr, scale)
             lr = common.directdownsample(hr, scale)
 
