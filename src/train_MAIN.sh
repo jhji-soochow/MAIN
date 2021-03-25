c9a2bc24e6f8cf825be56f6fea048d168dc07a65
jhji-soochow <jhji.soochow@gmail.com>
2021-03-25 10:39:23 +0800

    update training script

--- src/train_MAIN.sh
+++ src/train_MAIN.sh
@@ -1,9 +1,9 @@
 # 训练一个AIN 96 
-CUDA_VISIBLE_DEVICES=4,2 python main.py --trainer trainer --template AIN --n_GPUs 2 --lr 0.0001 --loss 1*MSE \
---data_train DIV2K --data_range 1-800/801-810 --n_colors 1 --scale 2 --batch_size=16 --patch_size 96 --direct_downsampling \
---data_test mySet5+myUrban12 \
---epochs 400 --print_every 100 --test_every 1000 --n_threads 16 \
---save AIN_96x2 --save_results --visdom --ext sep
+# CUDA_VISIBLE_DEVICES=4 python main.py --trainer trainer --template AIN --n_GPUs 1 --lr 0.0001 --loss 1*MSE \
+# --data_train DIV2K --data_range 1-800/801-810 --n_colors 1 --scale 2 --batch_size=16 --patch_size 96 --direct_downsampling \
+# --data_test mySet5+myUrban12 \
+# --epochs 400 --print_every 100 --test_every 1000 --n_threads 16 \
+# --save AIN_96x2 --save_results --visdom --ext sep
 
 # # 96 * sqrt(2) = 136
 # CUDA_VISIBLE_DEVICES=1 python main.py --trainer trainer --template AIN --n_GPUs 1 --lr 0.0001 --loss 1*MSE \
